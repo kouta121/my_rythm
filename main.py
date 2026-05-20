86af44acc0cd27d43bdba47eb4c3bcdf8fb59ea2
kouta121 <kouta.saho0126@gmail.com>
2026-05-21 00:14:06 +0900

    display judge

--- main.py
+++ main.py
@@ -1,148 +1,44 @@
 import pygame
 from pygame.locals import *
 import sys
-import time
-import json
-
-class Note:
-    def __init__(self, lane, timing, travel_time):
-        self.timing = timing
-        self.lane = lane
-        lane_x = {
-            0:200,
-            1:300,
-            2:400, 
-            3:500
-        }
-        self.x = lane_x[lane]
-        self.y = -50
-        self.speed = 5
-        self.hit = False
-        self.judge_time = travel_time + timing
-
-    def update(self, current_time):
-        if current_time >= self.timing:
-            self.y += self.speed
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 50, 50))
 
 def main():
-    pygame.init()
+    pygame.init()                                   # Pygameの初期化
+    screen = pygame.display.set_mode((800, 600))    # 800 x 600の大きさの画面を作る
+    pygame.display.set_caption("rythm")              # 画面上部に表示するタイトルを設定
 
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("rhythm")
+    x = 100
+    y = 100
 
-    clock = pygame.time.Clock()
-
-    lane_x = 350
-    notes = []
-
-    # JSONファイルからノートの情報を読み込む
-    with open('charts/test.json', 'r') as f:
-        note_data = json.load(f)
-        for data in note_data:
-            note = Note(data['lane'], data['timing'], 1830)  # ノートの位置とタイミングを設定
-            notes.append(note)
-
-    note_speed = 5
-
-    note_time = 2000
-
-    score = 0
-
-    combo = 0
-    max_combo = 0
-
-    pressed_lane = 0
-
-    judge_text = ""
-    judge_timer = 0
-
-    font = pygame.font.Font(r"font\ContiNeue2P-1.0.1.otf", 30)
-
+    lane_x = 300
+    lane_y = 600
+    lane_width = 100
+    lane_height = 100
 
     while True:
-
-        current_time = pygame.time.get_ticks()
-
-        if judge_timer > 0:
-            judge_timer -= 1
-        else:
-            judge_text = ""
-
+        # イベント処理
         for event in pygame.event.get():
-            if event.type == QUIT:
-                print(f"Final Score: {score}")
-                print(f"Max Combo: {max_combo}")
-                pygame.quit()
+            if event.type == QUIT:  # 閉じるボタンが押されたら終了
+                pygame.quit()       # Pygameの終了(画面閉じられる)
                 sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_d:
-                    pressed_lane = 0
-                elif event.key == K_f:
-                    pressed_lane = 1
-                elif event.key == K_j:
-                    pressed_lane = 2
-                elif event.key == K_k:
-                    pressed_lane = 3
-                    
-                for note in notes:
 
-                    if note.hit:
-                            continue
-                    if note.lane != pressed_lane:
-                            continue
+        notes = []
+        current_time = pygame.time.get_ticks()  # 現在の時間をミリ秒で取得
+        if current_time >= note_time:  # ノートの時間が来たら
+            note.visible = True  # ノートを表示する
 
-                    diff = abs(current_time - note.judge_time)
+        diff = abs(current_time - note.time)  # ノートの時間と現在の時間の差を計算
+        if diff <= 50:  # タイミングが50ms以内ならヒットとする
+            print("Perfect!")
+        if diff <= 100:  # タイミングが100ms以内ならグッドとする
+            print("Good!")
+        else:  # タイミングが100ms以上ならミスとする
+            print("Miss!")
 
-                    if diff <= 50:
-                        print("Perfect!")
-                        judge_text = "Perfect!"
-                        judge_timer = 30
-                        score += 100
-                        combo += 1
-                        if combo > max_combo:
-                            max_combo = combo
-                        note.hit = True
-                        break
+        screen.fill((0,0,0))        # 画面を黒色に塗りつぶし
+        pygame.draw.rect(screen, (255, 255, 255), (x, y, 50, 50))  # 赤色の四角形を描画
+        pygame.display.update()     # 画面を更新
 
-                    elif diff <= 100:
-                        print("Good!")
-                        judge_text = "Good!"
-                        judge_timer = 30
-                        score += 50
-                        combo += 1
-                        if combo > max_combo:
-                            max_combo = combo
-                        note.hit = True
-                        break
-            
-
-
-        for note in notes:
-
-            if current_time >= note.timing:
-                note.update(current_time)
-
-            if not note.hit and current_time > note.judge_time + 100:
-                print("Miss!")
-                judge_text = "Miss!"
-                judge_timer = 30
-                combo = 0
-                note.hit = True
-
-        screen.fill((0, 0, 0))
-        text = font.render(judge_text, True, (255, 255, 255))
-        screen.blit(text, (350, 400))
-
-        for note in notes:
-            if not note.hit:   
-                note.draw(screen)
-        pygame.draw.line(screen, (255, 0 , 0), (0, 500), (800, 500), 5)
-        pygame.display.update()
-
-        clock.tick(60)
 
 if __name__ == "__main__":
     main()
---
+++ sample.py
@@ -0,0 +1,148 @@
+import pygame
+from pygame.locals import *
+import sys
+import time
+import json
+
+class Note:
+    def __init__(self, lane, timing, travel_time):
+        self.timing = timing
+        self.lane = lane
+        lane_x = {
+            0:200,
+            1:300,
+            2:400, 
+            3:500
+        }
+        self.x = lane_x[lane]
+        self.y = -50
+        self.speed = 5
+        self.hit = False
+        self.judge_time = travel_time + timing
+
+    def update(self, current_time):
+        if current_time >= self.timing:
+            self.y += self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 50, 50))
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("rhythm")
+
+    clock = pygame.time.Clock()
+
+    lane_x = 350
+    notes = []
+
+    # JSONファイルからノートの情報を読み込む
+    with open('charts/test.json', 'r') as f:
+        note_data = json.load(f)
+        for data in note_data:
+            note = Note(data['lane'], data['timing'], 1830)  # ノートの位置とタイミングを設定
+            notes.append(note)
+
+    note_speed = 5
+
+    note_time = 2000
+
+    score = 0
+
+    combo = 0
+    max_combo = 0
+
+    pressed_lane = 0
+
+    judge_text = ""
+    judge_timer = 0
+
+    font = pygame.font.Font(r"font\ContiNeue2P-1.0.1.otf", 30)
+
+
+    while True:
+
+        current_time = pygame.time.get_ticks()
+
+        if judge_timer > 0:
+            judge_timer -= 1
+        else:
+            judge_text = ""
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                print(f"Final Score: {score}")
+                print(f"Max Combo: {max_combo}")
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_d:
+                    pressed_lane = 0
+                elif event.key == K_f:
+                    pressed_lane = 1
+                elif event.key == K_j:
+                    pressed_lane = 2
+                elif event.key == K_k:
+                    pressed_lane = 3
+                    
+                for note in notes:
+
+                    if note.hit:
+                            continue
+                    if note.lane != pressed_lane:
+                            continue
+
+                    diff = abs(current_time - note.judge_time)
+
+                    if diff <= 50:
+                        print("Perfect!")
+                        judge_text = "Perfect!"
+                        judge_timer = 30
+                        score += 100
+                        combo += 1
+                        if combo > max_combo:
+                            max_combo = combo
+                        note.hit = True
+                        break
+
+                    elif diff <= 100:
+                        print("Good!")
+                        judge_text = "Good!"
+                        judge_timer = 30
+                        score += 50
+                        combo += 1
+                        if combo > max_combo:
+                            max_combo = combo
+                        note.hit = True
+                        break
+            
+
+
+        for note in notes:
+
+            if current_time >= note.timing:
+                note.update(current_time)
+
+            if not note.hit and current_time > note.judge_time + 100:
+                print("Miss!")
+                judge_text = "Miss!"
+                judge_timer = 30
+                combo = 0
+                note.hit = True
+
+        screen.fill((0, 0, 0))
+        text = font.render(judge_text, True, (255, 255, 255))
+        screen.blit(text, (350, 400))
+
+        for note in notes:
+            if not note.hit:   
+                note.draw(screen)
+        pygame.draw.line(screen, (255, 0 , 0), (0, 500), (800, 500), 5)
+        pygame.display.update()
+
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
